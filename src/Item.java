f9cff18d6535d9a35f3054748d233e6db00f89cc
kalexei <aleksei.kovalev412@gmail.com>
2024-10-28 10:05:02 +0400

    init

---
+++ src/Item.java
@@ -0,0 +1,25 @@
+public class Item {
+	private String name;
+	private int price;
+
+	Item(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public double calculatePriceWithTax(double taxRate) {
+		if(taxRate > 1.00 || taxRate < 0.00) {
+			System.out.println("Invalid tax rate");
+			return price;
+		}
+		return price * (1.00 + taxRate);
+	}
+}
